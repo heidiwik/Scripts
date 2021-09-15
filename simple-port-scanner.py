91a8a6a83b499d7c9e1bbb55d401bfe4885e2877
Heidi Wikman <Heidi.Wikman@sulava.com>
2021-09-15 11:43:48 +0300

    fixed bug & added clear

--- simple-port-scanner.py
+++ simple-port-scanner.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 from datetime import datetime
 
+subprocess.call('cls', shell=True)
+
 remoteServer = input("Enter a remote host to scan: ")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
@@ -13,13 +15,15 @@ print("_" * 60)
 t1 = datetime.now()
 
 try:
-    for port in range (1,25):
-
+    for port in range (20,22):
+        print(".")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result ==0:
             print ("Port {}:        Open".format(port))
             sock.close()
+        else:
+            print(".")
 
 except KeyboardInterrupt:
     print("You pressed Ctrl+C")
@@ -37,4 +41,4 @@ t2 = datetime.now()
 
 total = t2 - t1 
 
-print("Scanning Completed in: "), total
+print("Scanning Completed in: ", total)
