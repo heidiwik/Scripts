8c1cf8fcc9e07fb41b555d3346ca0c193a77bbf6
Heidi Wikman <40593328+heidiwik@users.noreply.github.com>
2021-09-15 08:28:37 +0300

    Create simple-port-scanner.py

---
+++ simple-port-scanner.py
@@ -0,0 +1,40 @@
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+remoteServer = input("Enter a remote host to scan: ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+print("_" * 60)
+print("Please wait a while")
+print("_" * 60)
+
+t1 = datetime.now()
+
+try:
+    for port in range (1,25):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result ==0:
+            print ("Port {}:        Open".format(port))
+            sock.close()
+
+except KeyboardInterrupt:
+    print("You pressed Ctrl+C")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved. Exiting")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
+
+t2 = datetime.now()
+
+total = t2 - t1 
+
+print("Scanning Completed in: "), total
